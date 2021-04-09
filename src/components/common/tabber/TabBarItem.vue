93d6ca48586d5b0d82f907c520a27d655e74e631
baiqingdong <1455601142@qq.com>
2021-04-09 08:58:42 +0800

    '搭建项目路径结构'

---
+++ src/components/common/tabber/TabBarItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tab-bar-item" @click="tabClick">
+      <div v-if="!active"><slot name="item-icon"></slot></div>
+      <div v-else><slot name="item-active-icon"></slot></div>
+      <div :style="activeClass"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'TabBarItem',
+    props: {
+      path: String,
+      textColor: {
+        type: String,
+        default: 'red'
+      }
+    },
+    computed: {
+      active() {
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeClass() {
+        return this.active ? {color: this.textColor} : {}
+      }
+    },
+    methods: {
+        tabClick() {
+          this.$router.replace(this.path);
+        }
+    }
+}
+</script>
+
+<style>
+.tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+  }
+</style>
